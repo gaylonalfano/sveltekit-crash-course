efab69776aed7cc063659eb14cac0a8f13bfb12c
gaylonalfano <gaylon.alfano@gmail.com>
2021-11-16 21:11:19 +0800

    Working on deleting the feedback cars on button close click.

---
+++ src/lib/Card.svelte
@@ -0,0 +1,14 @@
+<div class="card">
+	<slot />
+</div>
+
+<style>
+	.card {
+		position: relative;
+		background-color: #fff;
+		color: #333;
+		border-radius: 15px;
+		padding: 40px 50px;
+		margin: 20px 0;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import FeedbackList from '$lib/FeedbackList.svelte';
+
+	let feedback: Record<string, any>[] = [
+		{
+			id: 1,
+			rating: 10,
+			text: 'Lolor sit amet consectetur adipisicing elit. Voluptatibus aperiam necessitatibus dolorem ut nostrum ratione qui, placeat blanditiis nulla suscipit corporis ullam quo dicta impedit ipsum autem id assumenda voluptatem.'
+		},
+		{
+			id: 2,
+			rating: 9,
+			text: 'Consectetur adipisicing elit. Voluptatibus aperiam necessitatibus dolorem ut nostrum ratione qui, placeat blanditiis nulla suscipit corporis ullam quo dicta impedit ipsum autem id assumenda voluptatem.'
+		},
+		{
+			id: 3,
+			rating: 6,
+			text: 'Sit amet consectetur adipisicing elit. Voluptatibus aperiam necessitatibus dolorem ut nostrum ratione qui, placeat blanditiis nulla suscipit corporis ullam quo dicta impedit ipsum autem id assumenda voluptatem.'
+		}
+	];
+</script>
+
+<div class="container">
+	<FeedbackList {feedback} />
+</div>
