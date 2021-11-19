6950bc0d507981edcf602fbb45fb2b18907ff4b7
gaylonalfano <gaylon.alfano@gmail.com>
2021-11-19 09:03:43 +0800

    Learned how to apply animations transitions. Super easy. Just wrap your custom component with a div and then add some premade attributes to the div tag like in:scale out:fade={{ duration: 500 }} stuff like that. Easy.

--- src/lib/feedback/FeedbackList.svelte
+++ src/lib/feedback/FeedbackList.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { fade, scale } from 'svelte/transition';
 	import FeedbackItem from '$lib/feedback/FeedbackItem.svelte';
 	export let feedback;
 
@@ -6,6 +7,9 @@
 </script>
 
 {#each feedback as fb (fb.id)}
-	<!-- NOTE Can dispatch/emit up to parent by leaving empty -->
-	<FeedbackItem item={fb} on:delete-feedback-item />
+	<!-- NOTE Add transitions by wrapping the custom component -->
+	<div in:scale out:fade={{ duration: 500 }}>
+		<!-- NOTE Can dispatch/emit up to parent by leaving empty -->
+		<FeedbackItem item={fb} on:delete-feedback-item />
+	</div>
 {/each}
